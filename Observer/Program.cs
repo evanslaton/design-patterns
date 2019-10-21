b6f7cfd8212322ba19b421e955e606e49f476042
Evan Slaton <evanslaton@gmail.com>
2019-10-20 21:13:13 -0700

    Finished observer pattern

--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ObserverPattern
+﻿using ObserverPattern.Models;
+
+namespace ObserverPattern
 {
     class Program
     {
@@ -7,6 +9,7 @@
             WeatherStation weatherStation = new WeatherStation();
             CurrentConditionsDisplay currentConditionsDisplay = new CurrentConditionsDisplay(weatherStation);
             StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherStation);
+            HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherStation);
             weatherStation.setMeasurements(80, 65, 30.4);
             weatherStation.setMeasurements(82, 70, 29.2);
             weatherStation.setMeasurements(78, 90, 29.2);
