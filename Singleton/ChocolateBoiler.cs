36b55587f252201cc7a14c3b69afb4bcdcb5a139
Evan Slaton <evanslaton@gmail.com>
2019-11-18 11:01:22 -0800

    Finished singleton pattern

---
+++ Singleton/ChocolateBoiler.cs
@@ -0,0 +1,53 @@
+﻿namespace Singleton
+{
+    public class ChocolateBoiler
+    {
+        private static ChocolateBoiler CHOCOLATE_BOILER = new ChocolateBoiler();
+        //For lazy instantiation -> can cause issues with multi-threading
+        //private static ChocolateBoiler CHOCOLATE_BOILER = null;
+        private bool Empty { get; set; }
+        private bool Boiled { get; set; }
+
+        private ChocolateBoiler()
+        {
+            Empty = true;
+            Boiled = false;
+        }
+
+        public static ChocolateBoiler GetInstance() => CHOCOLATE_BOILER;
+
+        //For lazy instantiation
+        //public static ChocolateBoiler GetInstance()
+        //{
+        //    if (CHOCOLATE_BOILER == null) CHOCOLATE_BOILER = new ChocolateBoiler();
+        //    return CHOCOLATE_BOILER;
+        //}
+
+        public void Fill()
+        {
+            if (Empty)
+            {
+                Empty = false;
+                Boiled = false;
+            }
+        }
+
+        public void Drain()
+        {
+            if (!Empty && Boiled)
+            {
+                Empty = true;
+            }
+        }
+
+        public void Boil()
+        {
+            if (!Empty && !Boiled)
+            {
+                Boiled = true;
+            }
+        }
+
+        public override string ToString() => $"\nEmpty: {Empty}\nBoiled: {Boiled}";
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler chocolateBoiler = ChocolateBoiler.GetInstance();
+            ChocolateBoiler chocolateBoiler2 = ChocolateBoiler.GetInstance();
+
+            Console.WriteLine("ChocolateBoiler:" + chocolateBoiler);
+            Console.WriteLine("ChocolateBoiler2:" + chocolateBoiler2);
+
+            chocolateBoiler.Fill();
+
+            Console.WriteLine("\nFILL:");
+            Console.WriteLine("ChocolateBoiler:" + chocolateBoiler);
+            Console.WriteLine("ChocolateBoiler2:" + chocolateBoiler2);
+
+            chocolateBoiler2.Boil();
+
+            Console.WriteLine("\nBOIL:");
+            Console.WriteLine("ChocolateBoiler:" + chocolateBoiler);
+            Console.WriteLine("ChocolateBoiler2:" + chocolateBoiler2);
+
+            chocolateBoiler.Drain();
+
+            Console.WriteLine("\nDRAIN:");
+            Console.WriteLine("ChocolateBoiler:" + chocolateBoiler);
+            Console.WriteLine("ChocolateBoiler2:" + chocolateBoiler2);
+        }
+    }
+}
