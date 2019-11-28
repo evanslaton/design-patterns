6834b0fff35c272a774841c2245e11019e8dce9d
Evan Slaton <evanslaton@gmail.com>
2019-11-28 15:08:10 -0800

    Set up menus for example

---
+++ Iterator/DinerMenu.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator
+{
+    public class DinerMenu
+    {
+        public static int MAX_ITEMS = 6;
+        public int NumberOfItems { get; set; }
+        MenuItem[] MenuItems { get; }
+
+        public DinerMenu()
+        {
+            MenuItems = new MenuItem[MAX_ITEMS];
+            AddItem("Vegetarian BLT", "Fakin\' with lettuce and tomato", true, 2.99);
+            AddItem("BLT", "Bacon with lettuc and tomato", false, 2.99);
+            AddItem("Soup of the day", "Soup with a side of potato salade", false, 3.29);
+        }
+
+        public void AddItem(string name, string description,
+                    bool vegetarian, double price)
+        {
+            MenuItem menuItem = new MenuItem(name, description, vegetarian, price);
+            if (NumberOfItems >= MAX_ITEMS)
+            {
+                Console.WriteLine("Sorry, menu is full! Can't add item to menu.");
+            }
+            else
+            {
+                MenuItems[NumberOfItems] = menuItem;
+                NumberOfItems++;
+            }
+        }
+    }
+}
---
+++ Iterator/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Iterator
+{
+    public class MenuItem
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public bool Vegetarian { get; }
+        public double Price { get; }
+
+        public MenuItem(string name, string description,
+                        bool vegetarian, double price)
+        {
+            Name = name;
+            Description = description;
+            Vegetarian = vegetarian;
+            Price = price;
+        }
+    }
+}
