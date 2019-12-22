29e4cd7da6e8ada59ae17b9616801e83b5e0224b
Evan Slaton <evanslaton@gmail.com>
2019-12-21 17:09:26 -0800

    Finished composite pattern

--- Composite/MenuCompmonent.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Iterator
-{
-    internal class MenuCompmonent
-    {
-    }
-}
--- Composite/Program.cs
+++ Composite/Program.cs
@@ -8,8 +8,23 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            IMenu[] menus = { new PancakeHouseMenu(), new DinerMenu(), new CafeMenu() };
-            Waiter waiter = new Waiter(menus);
+            MenuComponent pancakeHouseMenu = new Menu("PANCAKE HOUSE MENU", "Breakfast");
+            MenuComponent dinerMenu = new Menu("DINER MENU", "Lunch");
+            MenuComponent cafeMenu = new Menu("CAFE MENU", "Dinner");
+            MenuComponent dessertMenu = new Menu("DESSERT MENU", "Dessert of course!");
+
+            MenuComponent allMenus = new Menu("ALL MENUS", "All menus combined");
+
+            allMenus.Add(pancakeHouseMenu);
+            allMenus.Add(dinerMenu);
+            allMenus.Add(cafeMenu);
+
+            dinerMenu.Add(new MenuItem("Pasta", "Spaghetti with marinara sauce", true, 3.89));
+
+            dinerMenu.Add(dessertMenu);
+            dessertMenu.Add(new MenuItem("Apple Pie", "Apples and cinnamon", true, 1.59));
+
+            Waiter waiter = new Waiter(allMenus);
 
             waiter.PrintMenu();
         }
